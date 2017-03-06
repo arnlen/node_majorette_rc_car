3b1b466e05e0aac82e87c4d76ece4849b2a8a8de
Arnaud Lenglet <arlenglet@gmail.com>
2017-03-06 14:28:29 +0100

    refactor(motors): split motor logic into separate module

--- main.js
+++ main.js
@@ -7,81 +7,32 @@ io = require('socket.io').listen(server);    //web socket server
 // Node server start
 server.listen(8080); //start the webserver on port 8080
 app.use(express.static('public')); //tell the server that ./public/ contains the static webpages
-console.log("running");
+console.log("Server running");
 
-// Config
-var motor1EnablePin = 33; //pwm
-var motor1Input1Pin = 12;
-var motor1Input2Pin = 16;
+var motors = require("./motors.js");
+motors.initMotorsPins();
+console.log("Motors pins initialized");
 
-var motor2EnablePin = 35; //pwm
-var motor2Input1Pin = 18;
-var motor2Input2Pin = 22;
+// --- --- --- ---
 
-// RPIO Setup and pin init
-var rpio = require('rpio');
+io.sockets.on('connection', function (socket) {
+    console.log("New client connected!");
 
-rpio.open(motor1EnablePin, rpio.OUTPUT, rpio.LOW);          // Enable 1
-rpio.open(motor2EnablePin, rpio.OUTPUT, rpio.LOW);          // Enable 2
+    // Send to the new client the current speed of motors
+    socket.emit('motor1', { value: motors.getMotorSpeed(1) });
+    socket.emit('motor2', { value: motors.getMotorSpeed(2) });
 
-rpio.open(motor1Input1Pin, rpio.OUTPUT, rpio.LOW);       // Motor 1, Input 1
-rpio.open(motor1Input2Pin, rpio.OUTPUT, rpio.LOW);       // Motor 1, Input 2
-rpio.open(motor2Input1Pin, rpio.OUTPUT, rpio.LOW);       // Motor 2, Input 1
-rpio.open(motor2Input2Pin, rpio.OUTPUT, rpio.LOW);       // Motor 2, Input 2
-
-// TEST RUN
-// rpio.write(motor1EnablePin, rpio.HIGH);
-// console.log("Motor 1 Enable: " + rpio.read(motor1EnablePin));
-// rpio.write(motor2EnablePin, rpio.HIGH);
-// console.log("Motor 2 Enable: " + rpio.read(motor2EnablePin));
-
-// rpio.write(motor1Input1Pin, rpio.HIGH);
-// console.log("Motor 1 Input 1: " + rpio.read(motor1Input1Pin));
-// rpio.write(motor1Input2Pin, rpio.LOW);
-// console.log("Motor 1 Input 2: " + rpio.read(motor1Input2Pin));
-
-// rpio.write(motor2Input1Pin, rpio.HIGH);
-// console.log("Motor 2 Input 1: " + rpio.read(motor2Input1Pin));
-// rpio.write(motor2Input2Pin, rpio.LOW);
-// console.log("Motor 2 Input 2: " + rpio.read(motor2Input2Pin));
-
-
-var speed = 0; //static variable to hold the current speed
-io.sockets.on('connection', function (socket) { //gets called whenever a client connects
-    socket.emit('motor1', {value: speed}); //send the new client the current speed
-    console.log("connected");
-
-    socket.on('motor1', function (data) { //makes the socket react to 'motor1' packets by calling this function
-        rpio.write(motor1EnablePin, rpio.HIGH);
-        speed = data.value;  //updates speed from the data object
-
-        if (speed > 0) {
-          rpio.write(motor1Input1Pin, rpio.HIGH);
-          rpio.write(motor1Input2Pin, rpio.LOW);
-        } else if (speed < 0) {
-          rpio.write(motor1Input1Pin, rpio.LOW);
-          rpio.write(motor1Input2Pin, rpio.HIGH);
-        } else {
-          rpio.write(motor1EnablePin, rpio.LOW);
-        }
-
-        io.sockets.emit('motor1', {value: speed}); //sends the updated speed to all connected clients
+    socket.on('motor1', function (data) {
+      var motorId = 1;
+      motors.runMotor(motorId, data.value);
+      // Send the new speed to all connected clients
+      io.sockets.emit('motor' + motorId, { value: motors.getMotorSpeed(motorId) });
     });
 
-    socket.on('motor2', function (data) { //makes the socket react to 'motor1' packets by calling this function
-        rpio.write(motor2EnablePin, rpio.HIGH);
-        speed = data.value;  //updates speed from the data object
-
-        if (speed > 0) {
-          rpio.write(motor2Input1Pin, rpio.HIGH);
-          rpio.write(motor2Input2Pin, rpio.LOW);
-        } else if (speed < 0) {
-          rpio.write(motor2Input1Pin, rpio.LOW);
-          rpio.write(motor2Input2Pin, rpio.HIGH);
-        } else {
-          rpio.write(motor2EnablePin, rpio.LOW);
-        }
-
-        io.sockets.emit('motor2', {value: speed}); //sends the updated speed to all connected clients
+    socket.on('motor2', function (data) {
+      var motorId = 2;
+      motors.runMotor(motorId, data.value);
+      // Send the new speed to all connected clients
+      io.sockets.emit('motor' + motorId, { value: motors.getMotorSpeed(motorId) });
     });
 });
---
+++ motors.js
@@ -0,0 +1,114 @@
+// Config
+var motor1EnablePin = 33;   // pwm
+var motor1Input1Pin = 12;
+var motor1Input2Pin = 16;
+
+var motor2EnablePin = 35;   // pwm
+var motor2Input1Pin = 18;
+var motor2Input2Pin = 22;
+
+// Store motors' speed
+var motor1Speed = 0;
+var motor2Speed = 0;
+
+// RPIO Setup and pin init
+var rpio = require('rpio');
+
+// --- --- PUBLIC METHODS --- ---
+
+exports.initMotorsPins = function() {
+  rpio.open(motor1EnablePin, rpio.OUTPUT, rpio.LOW);       // Enable 1
+  rpio.open(motor2EnablePin, rpio.OUTPUT, rpio.LOW);       // Enable 2
+
+  rpio.open(motor1Input1Pin, rpio.OUTPUT, rpio.LOW);       // Motor 1, Input 1
+  rpio.open(motor1Input2Pin, rpio.OUTPUT, rpio.LOW);       // Motor 1, Input 2
+  rpio.open(motor2Input1Pin, rpio.OUTPUT, rpio.LOW);       // Motor 2, Input 1
+  rpio.open(motor2Input2Pin, rpio.OUTPUT, rpio.LOW);       // Motor 2, Input 2
+};
+
+exports.runMotor = function(motorId, speed) {
+  activateMotor(motorId);
+  setMotorSpeed(motorId, speed);
+
+  if (speed > 0) {
+    runForward(motorId, Math.abs(speed));
+  } else if (speed < 0) {
+    runBackward(motorId, Math.abs(speed));
+  } else {
+    deactivateMotor(motorId);
+  }
+}
+
+exports.getMotorSpeed = function(motorId) {
+  switch (motorId) {
+    case 1:
+      return motor1Speed;
+      break;
+    case 2:
+      return motor2Speed;
+      break;
+  }
+}
+
+// --- --- PRIVATE METHODS --- ---
+
+var activateMotor = function(motorId) {
+  switch (motorId) {
+    case 1:
+      rpio.write(motor1EnablePin, rpio.HIGH);
+      break;
+    case 2:
+      rpio.write(motor2EnablePin, rpio.HIGH);
+      break;
+  }
+}
+
+var deactivateMotor = function(motorId) {
+  switch (motorId) {
+    case 1:
+      rpio.write(motor1EnablePin, rpio.LOW);
+      break;
+    case 2:
+      rpio.write(motor2EnablePin, rpio.LOW);
+      break;
+  }
+}
+
+var setMotorSpeed = function(motorId, speed) {
+  switch (motorId) {
+    case 1:
+      motor1Speed = speed;
+      break;
+    case 2:
+      motor2Speed = speed;
+      break;
+  }
+}
+
+// TODO: Make use of speed
+var runForward = function(motorId, absoluteSpeed) {
+  switch (motorId) {
+    case 1:
+      rpio.write(motor1Input1Pin, rpio.HIGH);
+      rpio.write(motor1Input2Pin, rpio.LOW);
+      break;
+    case 2:
+      rpio.write(motor2Input1Pin, rpio.HIGH);
+      rpio.write(motor2Input2Pin, rpio.LOW);
+      break;
+  }
+}
+
+// TODO: Make use of speed
+var runBackward = function(motorId, absoluteSpeed) {
+  switch (motorId) {
+    case 1:
+      rpio.write(motor1Input1Pin, rpio.LOW);
+      rpio.write(motor1Input2Pin, rpio.HIGH);
+      break;
+    case 2:
+      rpio.write(motor2Input1Pin, rpio.LOW);
+      rpio.write(motor2Input2Pin, rpio.HIGH);
+      break;
+  }
+}
